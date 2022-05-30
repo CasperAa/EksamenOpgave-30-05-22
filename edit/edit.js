473750531e9a213d9ee71751db0e7efe40ee149f
CasperAa <caspercasper.aakviststeengaard@gmail.com>
2022-05-30 23:15:41 +0200

    More funktions added

--- edit/edit.js
+++ edit/edit.js
@@ -8,6 +8,8 @@ export function editHandelsers(match){
     const racerId = match.params.id;
     showRacerbasedOnID(racerId);
     document.getElementById("submit-btn").addEventListener("click", editRacer)
+    document.getElementById("delete-btn").addEventListener("click", deleteRacer)
+
 }
 
 
@@ -35,12 +37,20 @@ async function editRacer(){
     racerToAdd.country = document.getElementById("country").value
     racerToAdd.age = document.getElementById("age").value
     racerToAdd.points = document.getElementById("points").value
-    console.log(racerToAdd)
     try{
         await fetch(racerURL+"/"+racerId, makeOptions("PUT", racerToAdd)).then(res => handleErrors (res))
         
         }catch{ 
-            console.log(error.messange)
+            //console.log(error.messange)
+        }
+}
+
+async function deleteRacer(){
+    const racerId = document.getElementById("id").value
+    try{
+        await fetch(racerURL+"/"+racerId, makeOptions("DELETE")).then(res => handleErrors (res))
+        }catch{ 
+            //console.log(error.message)
         }
 }
 
--- racers/racer.js
+++ racers/racer.js
@@ -2,10 +2,14 @@ import { handleErrors } from "../fetchUtils.js";
 import { URL } from "../settings.js";
 
 const racerURL = URL + "/racers"
+const teamURL = URL + "/teams"
 
 
 export function racerHandelsers(){
-    showAllRacers();
+    showAllRacers()
+    showAllTeams()
+    document.getElementById("search-btn").addEventListener("click", showAllRacersByTeam)
+    
 }
 
 
@@ -24,6 +28,7 @@ async function showAllRacers(){
             <td>${racer.points}</td>
             <td class="edit-btn"> <a class="btn btn-primary" href="#/EditRacer?id=${racer.id}" data-navigo> 
             Edit </td>
+        
         </tr>
         `).join("\n")
         document.getElementById("tbl-body").innerHTML = tableData
@@ -32,3 +37,47 @@ async function showAllRacers(){
     }
     
 }
+
+async function showAllTeams(){
+    try{
+    const teams = await fetch(teamURL).then(res => handleErrors (res))
+        console.log(teams)
+        const allTeams = teams.map(team => `
+                
+                <option> ${team.name} </option>    
+      
+        `).join("\n")
+        document.getElementById("teams").innerHTML = allTeams
+    }catch{
+        //console.log(error.messange)
+    }
+}
+
+
+async function showAllRacersByTeam(){
+    document.getElementById("tbl-body").innerHTML = ""
+    const searchTeam = document.getElementById("teams").value
+    try{
+    const racer = await fetch(racerURL + "?team=" + searchTeam).then(res => handleErrors (res))
+        console.log(racerURL + "?team=" + searchTeam)
+        const tableData = racer.map(racer => `
+        <tr> 
+            <td>${racer.id}</td>    
+            <td>${racer.firstName}</td>
+            <td>${racer.lastName}</td>
+            <td>${racer.team}</td>
+            <td>${racer.country}</td>
+            <td>${racer.age}</td>
+            <td>${racer.points}</td>
+            <td class="edit-btn"> <a class="btn btn-primary" href="#/EditRacer?id=${racer.id}" data-navigo> 
+            Edit </td>
+        
+        </tr>
+        `).join("\n")
+        document.getElementById("tbl-body").innerHTML = tableData
+    }catch{
+        console.log(error.messange)
+    }
+    
+}
+
