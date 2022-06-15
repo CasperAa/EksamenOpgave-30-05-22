eac351c84dace4a8e874493db5721ac2b4d343b1
CasperAa <caspercasper.aakviststeengaard@gmail.com>
2022-06-15 18:44:05 +0200

    Added api to url

--- settings.js
+++ settings.js
@@ -1,2 +1,2 @@
-export const URL ="https://technology-exam-summer-22.azurewebsites.net"
+export const URL ="https://technology-exam-summer-22.azurewebsites.net/api"
 export const localHost = "http://localhost:8080/api"
